2e06a6398e20d4c9450f6e6d45c766d8ad70d643
JaegerMa <JaegerMa.Git@lightning-mail.de>
2018-11-12 17:31:45 +0100

    Add .FromDictionary method to INIFile

--- SharpINI/INIFile.cs
+++ SharpINI/INIFile.cs
@@ -20,5 +20,46 @@ namespace SharpINI
 			}
 			set => base[key] = value;
 		}
+
+
+
+		public static INIFile FromDictionary(IDictionary<string, IDictionary<string, string>> dictionary, bool deepClone = true)
+		{
+			IEnumerable<(string, IDictionary<string, string>)> entries;
+			if(deepClone)
+				entries = GetClonedEntries();
+			else
+				entries = GetEntries();
+
+
+			var iniFile = new INIFile();
+
+			//We need to access the original index-setter
+			//from IDictionary as this allows IDictionary<string, string> to be set
+			//while the one from INIFile only accepts INISections
+			IDictionary<string, IDictionary<string, string>> iniDictionary = iniFile;
+			foreach(var (key, value) in entries)
+				iniDictionary[key] = value;
+
+
+			return iniFile;
+
+
+
+			IEnumerable<(string, IDictionary<string, string>)> GetEntries()
+			{
+				foreach(var entry in dictionary)
+					yield return (entry.Key, entry.Value);
+			}
+			IEnumerable<(string, IDictionary<string, string>)> GetClonedEntries()
+			{
+				foreach(var entry in dictionary)
+				{
+					var section = new INISection(entry.Value);
+
+					yield return (entry.Key, section);
+				}
+			}
+		}
 	}
 }
